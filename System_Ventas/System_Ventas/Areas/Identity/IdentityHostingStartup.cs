29b9355836164b19718b1f472fc35f188e8ec700
Leandro Escalera <marcelo.escalera.inf@gmail.com>
2019-10-09 23:54:19 -0400

    Commit 09.10.2019
    Curso de Udemy hasta el video 5 de la segunda parte

---
+++ System_Ventas/System_Ventas/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System_Ventas.Data;
+
+[assembly: HostingStartup(typeof(System_Ventas.Areas.Identity.IdentityHostingStartup))]
+namespace System_Ventas.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ System_Ventas/System_Ventas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace System_Ventas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
